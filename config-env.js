b5b88d198ee2987d1e318fa2e452377a465586ac
Sandeepamax <udarasandeepa17@gmail.com>
2024-07-01 09:51:34 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
+    SESSION_ID:  process.env.SESSION_ID,"PRABATH-MD~PLBRxKaS#TZ_5i2KRemZQHog8cfvb46LaOtcNoMerPG-nTjQQARM"
+    BOT_NUMBER:  process.env.BOT_NUMBER,"94786215965"
     GITHUB_USERNAME: username,
     GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
 };
